a96e1ab21a9623aa6a167fd3a87f1ebad2300c0a
Gabriel442018 <37821374+Gabriel4420@users.noreply.github.com>
2022-01-10 17:10:07 -0300

    liberando acesso ao cors

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ class App {
     this.app.use(morgan('dev'))
     this.app.use((req, res, next) => {
       res.header('Access-Control-Allow-Origin', '*')
-      res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE')
+      res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,PATCH,OPTIONS')
       res.header(
         'Access-Control-Allow-Headers',
         'Access,Content-type,Authorization,Acept,Origin,X-requested-With',
