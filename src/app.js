48528069c4165197b96a48206e70512087abbc97
Gabriel442018 <37821374+Gabriel4420@users.noreply.github.com>
2022-01-11 04:27:17 -0300

    setando header corretamente

--- src/app.js
+++ src/app.js
@@ -19,10 +19,10 @@ class App {
     this.app.use(express.json())
     this.app.use(morgan('dev'))
     this.app.use((req, res, next) => {
-      res.set("Access-Control-Allow-Origin", "*");
-      res.set("Access-Control-Allow-Credentials", "true");
-      res.set("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT, PATCH");
-      res.set("Access-Control-Allow-Headers", "*");
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader("Access-Control-Allow-Credentials", "true");
+      res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT, PATCH");
+      res.setHeader("Access-Control-Allow-Headers", "*");
       //Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization
       this.app.use(cors('*'))
       next()
