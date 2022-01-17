08933378e80a49f2e03da925a786f8fd72fc764f
Gabriel442018 <37821374+Gabriel4420@users.noreply.github.com>
2022-01-17 15:55:16 -0300

    atualizando permissões de metodos do CORS

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ class App {
     this.app.use((req, res, next) => {
       res.setHeader("Access-Control-Allow-Origin", "*");
       res.setHeader("Access-Control-Allow-Credentials", "true");
-      res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT, PATCH");
+      res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE, PATCH");
       res.setHeader("Access-Control-Allow-Headers", "*");
       //Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization
       this.app.use(cors('*'))
